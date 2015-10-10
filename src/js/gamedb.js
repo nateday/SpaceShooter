5848e7ca6cf4dc6afb91cef9ed9fc612b1f65617
javaday <javaday@gmail.com>
2015-10-09 23:00:07 -0600

    Updated DB

--- src/js/gamedb.js
+++ src/js/gamedb.js
@@ -9,6 +9,37 @@ module.exports = (function() {
         self.db = new PouchDB('SpaceShooter');
     };
     
+    GameDB.prototype.getPlayerData = function(dataCallback, errCallback) {
+        
+        var self = this;
+        
+        self.db.get('player')
+            .then(function (player) {
+                dataCallback(player);
+            })
+            .catch(function(error) {
+                errCallback(error);
+            });    
+    };
+    
+    GameDB.prototype.savePlayerData = function(data, dataCallback, errCallback) {
+        
+        var self = this;
+        
+        data._id = 'player';
+        
+        self.db.put(data)
+            .then(function() {
+                return self.db.get('player');
+            })
+            .then(function(player) {
+                dataCallback(player);
+            })
+            .catch(function(err) {
+                errCallback(err);
+            });   
+    };
+    
     return GameDB;
     
 })();
