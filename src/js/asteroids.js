5195568b5ea74caaf392ecf6f5257faf77b26129
nateday <samday@Sams-MacBook-Air.local>
2015-10-09 18:34:42 -0600

    Created Astroids

---
+++ src/js/asteroids.js
@@ -0,0 +1,22 @@
+module.exports = (function() {
+    
+    var sprites = null;
+    var game = null;
+    
+    var Asteroids = function(game) {
+        
+        this.game = game;
+        this.sprites = game.add.group();
+        
+        for (var i = 0; i < 5; i++) {
+            
+            var asteroid = this.game.add.sprite(100, 0, 'asteroid-32x32');
+            game.physics.enable(asteroid, Phaser.Physics.ARCADE);
+            asteroid.body.bounce.setTo(0.5, 0.5);
+            this.sprites.add(asteroid, true);
+        }
+    }
+    
+    return Asteroids;
+    
+})();
