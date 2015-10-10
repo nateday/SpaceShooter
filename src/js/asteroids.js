2f96f68a21c86497b71342efdd57e8fe4e563a1f
nateday <samday@Sams-MacBook-Air.local>
2015-10-09 22:14:25 -0600

    Added velocity to asteroid.

--- src/js/asteroids.js
+++ src/js/asteroids.js
@@ -12,6 +12,7 @@ module.exports = (function() {
             
             var asteroid = this.game.add.sprite(100, 0, 'asteroid-32x32');
             game.physics.enable(asteroid, Phaser.Physics.ARCADE);
+            asteroid.body.velocity.setTo(5 + i * 5, 7 + i * 20);
             asteroid.body.bounce.setTo(0.5, 0.5);
             this.sprites.add(asteroid, true);
         }
